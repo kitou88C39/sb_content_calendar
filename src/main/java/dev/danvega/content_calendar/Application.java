47a4ff15753feca27bd4ef437b54b29e9015a35d
kitou88C39 <kanfutrooper@icloud.com>
2023-05-23 13:08:45 +0900

    Message.java作成

--- src/main/java/dev/danvega/content_calendar/Application.java
+++ src/main/java/dev/danvega/content_calendar/Application.java
@@ -2,12 +2,14 @@ package dev.danvega.content_calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
